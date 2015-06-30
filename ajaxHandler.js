626f24273c7d71094802dc90691e926f64d6d708
Kishor Mali <kishor10d@gmail.com>
2015-06-30 19:11:27 +0530

    ajaxHandler
    This is generic ajax request handler

---
+++ ajaxHandler.js
@@ -0,0 +1,28 @@
+/**
+ * @author Kishor Mali
+ */
+
+/**
+ * This is generic ajax request handler to handle all ajax request
+ * 
+ * @param {Object} callUrl : This is url of function to be called
+ * @param {Object} postData : This is data to be post or get
+ * @param {Object} method : post or get method
+ * @param {Object} asyncStatus : async status TRUE or FALSE
+ * @param {Object} successCallback : success state handler
+ */
+var ajaxHandler = function(callUrl, postData, method, asyncStatus,  successCallback) {
+	
+	jQuery.ajax({
+		url : callUrl,
+		data : JSON.stringify(postData),
+		type : method,
+		async : asyncStatus,
+		success : successCallback,
+		error: function(xhr, textStatus, errorThrown) {
+			if(xhr.status = 400){
+				console.log('Method Not Found');
+			}
+        }
+	});
+};
