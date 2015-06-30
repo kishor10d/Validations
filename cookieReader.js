0fd42c3307d0bd6b399b2805aa5fdaa14e72c423
Kishor Mali <kishor10d@gmail.com>
2015-06-30 19:14:50 +0530

    cookieReader
    This is cookieReader, whatever cookies are in your browser, all prints in key value pair.

---
+++ cookieReader.js
@@ -0,0 +1,40 @@
+/**
+ * @author Kishor Mali
+ */
+ 
+
+/**
+ * This is generic cookie reader
+ */
+var cookieReader = function(){
+	
+	c = document.cookie.split('; ');
+    cookies = {};
+
+    for(i=c.length-1; i>=0; i--)
+    {
+       C = c[i].split('=');
+       cookies[C[0]] = C[1];
+    }
+
+    return cookies;
+};
+
+/**
+ * To check the read cookies
+ * 
+ * @param {Object} cookieString
+ */
+var printAllCookies = function(cookieString){
+	
+	console.log(cookieString);
+	
+	for(key in cookieString){
+		console.log("key : "+ key +   "   value : " + decodeURIComponent(cookieString[key]));
+	}
+};
+
+
+var cookier = cookieReader();
+
+printAllCookies(cookier);
